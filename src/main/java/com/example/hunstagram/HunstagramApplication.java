36aacd9b78f4687579c0fdbddc037300a6be7e6c
HunSeongPark <phunseong@gmail.com>
2022-11-02 19:07:55 +0900

    initial commit

---
+++ src/main/java/com/example/hunstagram/HunstagramApplication.java
@@ -0,0 +1,13 @@
+package com.example.hunstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HunstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hunstagram/HunstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hunstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HunstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
