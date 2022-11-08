77fb87f82ce7350221d353a74974162715f48999
HunSeongPark <phunseong@gmail.com>
2022-11-08 20:24:48 +0900

    add: User Controller

---
+++ src/main/java/com/example/hunstagram/domain/user/controller/UserApiController.java
@@ -0,0 +1,16 @@
+package com.example.hunstagram.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-11-08
+ */
+@RequiredArgsConstructor
+@RequestMapping("/v1/users")
+@RestController
+public class UserApiController {
+
+}
