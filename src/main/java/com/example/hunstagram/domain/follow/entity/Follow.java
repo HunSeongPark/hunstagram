b3be9478bb99c3b2d468c1579fe37f63b6e5d0a2
HunSeongPark <phunseong@gmail.com>
2022-11-08 21:22:07 +0900

    feat: Follow Entity

---
+++ src/main/java/com/example/hunstagram/domain/follow/entity/Follow.java
@@ -0,0 +1,34 @@
+package com.example.hunstagram.domain.follow.entity;
+
+import com.example.hunstagram.domain.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-11-08
+ */
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "follow_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "from_user_id")
+    private User fromUser;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "to_user_id")
+    private User toUser;
+}
