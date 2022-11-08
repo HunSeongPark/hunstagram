aae5f392720a06843d6e019ce9b71ac600019aaa
HunSeongPark <phunseong@gmail.com>
2022-11-08 20:21:46 +0900

    add: User Repository

---
+++ src/main/java/com/example/hunstagram/domain/user/entity/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.hunstagram.domain.user.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-11-08
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+}
