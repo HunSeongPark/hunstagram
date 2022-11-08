61ff6aed1d0b83456f099bd863fbf50c72f394bc
HunSeongPark <phunseong@gmail.com>
2022-11-08 21:53:30 +0900

    feat: User Entity null 여부 설정

--- src/main/java/com/example/hunstagram/domain/user/entity/User.java
+++ src/main/java/com/example/hunstagram/domain/user/entity/User.java
@@ -21,13 +21,18 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
+    @Column(name = "profile_image")
     private String profileImage;
 
+    @Column(name = "nickname", nullable = false)
     private String nickname;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "intro_text")
     private String introText;
 
+    @Column(name = "refresh_token")
     private String refreshToken;
 }
