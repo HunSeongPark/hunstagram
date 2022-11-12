8dc385ce70f28969f8ce96dfec80bc4e50e81526
HunSeongPark <phunseong@gmail.com>
2022-11-12 21:07:41 +0900

    feat: ErrorResponse에 Getter 추가

--- src/main/java/com/example/hunstagram/domain/user/controller/UserApiController.java
+++ src/main/java/com/example/hunstagram/domain/user/controller/UserApiController.java
@@ -1,9 +1,15 @@
 package com.example.hunstagram.domain.user.controller;
 
 import com.example.hunstagram.domain.user.service.UserService;
+import com.example.hunstagram.global.exception.CustomErrorCode;
+import com.example.hunstagram.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 /**
  * @author : Hunseong-Park
@@ -15,4 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApiController {
 
     private final UserService userService;
+
+    @GetMapping("/err")
+    public ResponseEntity<String> err() {
+        throw new CustomException(CustomErrorCode.TEST_ERROR);
+    }
+
+    @GetMapping("/err2")
+    public ResponseEntity<String> err2() {
+        try {
+            throw new SQLException("SQL EXCEPTION OCCUR");
+    } catch (SQLException e) {
+            throw new CustomException(CustomErrorCode.TEST_ERROR, e);
+        }
+    }
+
+    @GetMapping("/err3")
+    public ResponseEntity<String> err3() {
+        throw new CustomException(CustomErrorCode.TEST_ERROR, "테스트 에러 코드");
+    }
 }
--- src/main/java/com/example/hunstagram/global/exception/ErrorResponse.java
+++ src/main/java/com/example/hunstagram/global/exception/ErrorResponse.java
@@ -1,9 +1,12 @@
 package com.example.hunstagram.global.exception;
 
+import lombok.Getter;
+
 /**
  * @author : Hunseong-Park
  * @date : 2022-11-12
  */
+@Getter
 public class ErrorResponse {
     private final CustomErrorCode errorCode;
     private final String errorMessage;
