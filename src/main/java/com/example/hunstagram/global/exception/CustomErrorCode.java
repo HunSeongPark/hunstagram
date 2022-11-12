476c559f207c7d432f2f88e7f0e4cf16f0416564
HunSeongPark <phunseong@gmail.com>
2022-11-12 20:30:40 +0900

    feat: CustomErrorCode

---
+++ src/main/java/com/example/hunstagram/global/exception/CustomErrorCode.java
@@ -0,0 +1,17 @@
+package com.example.hunstagram.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-11-12
+ */
+@Getter
+@RequiredArgsConstructor
+public enum CustomErrorCode {
+    ;
+    private final HttpStatus httpStatus;
+    private final String errorMessage;
+}
