e1cc45df10eb564e3bfcaf10bb2d29a1e0f3e93c
HunSeongPark <phunseong@gmail.com>
2022-11-14 14:29:36 +0900

    feat: UserService - id,pw signup 로직 구현

--- src/main/java/com/example/hunstagram/global/exception/CustomErrorCode.java
+++ src/main/java/com/example/hunstagram/global/exception/CustomErrorCode.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.*;
+
 /**
  * @author : Hunseong-Park
  * @date : 2022-11-12
@@ -11,7 +13,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CustomErrorCode {
-    /* WRITE ErrorCode HERE! */;
+    EMAIL_ALREADY_EXISTS(BAD_REQUEST, "이미 존재하는 이메일입니다.");
     private final HttpStatus httpStatus;
     private final String errorMessage;
 }
