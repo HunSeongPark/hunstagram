e595f3de8410ecaf6e26cb7d4915d39d996a977e
HunSeongPark <phunseong@gmail.com>
2022-11-14 14:04:49 +0900

    feat: UserController - id, pw 회원가입 엔드포인트 추가

--- src/main/java/com/example/hunstagram/domain/user/controller/UserApiController.java
+++ src/main/java/com/example/hunstagram/domain/user/controller/UserApiController.java
@@ -1,15 +1,12 @@
 package com.example.hunstagram.domain.user.controller;
 
 import com.example.hunstagram.domain.user.service.UserService;
-import com.example.hunstagram.global.exception.CustomErrorCode;
-import com.example.hunstagram.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.SQLException;
 
 /**
  * @author : Hunseong-Park
@@ -22,4 +19,8 @@ public class UserApiController {
 
     private final UserService userService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto.SignUpResponse> signup(@RequestBody UserDto.SignUpRequest requestDto) {
+        return ResponseEntity.ok(userService.signup(requestDto));
+    }
 }
