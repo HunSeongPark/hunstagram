ac54eb6e6e5f06c69eac98f5027a554778bd23e4
HunSeongPark <phunseong@gmail.com>
2022-11-14 20:09:39 +0900

    refactor: email, nickname 중복확인 로직 메서드 추출

--- src/main/java/com/example/hunstagram/domain/user/service/UserService.java
+++ src/main/java/com/example/hunstagram/domain/user/service/UserService.java
@@ -2,14 +2,14 @@ package com.example.hunstagram.domain.user.service;
 
 import com.example.hunstagram.domain.user.dto.UserDto;
 import com.example.hunstagram.domain.user.entity.UserRepository;
-import com.example.hunstagram.global.exception.CustomErrorCode;
 import com.example.hunstagram.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.example.hunstagram.global.exception.CustomErrorCode.*;
+import static com.example.hunstagram.global.exception.CustomErrorCode.EMAIL_ALREADY_EXISTS;
+import static com.example.hunstagram.global.exception.CustomErrorCode.NICKNAME_ALREADY_EXISTS;
 
 /**
  * @author : Hunseong-Park
@@ -25,17 +25,26 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public UserDto.SignUpResponse signup(UserDto.SignUpRequest requestDto) {
-        if (userRepository.existsByEmail(requestDto.getEmail())) {
-            throw new CustomException(EMAIL_ALREADY_EXISTS);
-        }
+        validateDuplicateEmail(requestDto.getEmail());
         return UserDto.SignUpResponse.fromRequestDto(requestDto);
     }
 
-    public void signupInfo(UserDto.SignUpInfoRequest requestDto) {
-        if (userRepository.existsByNickname(requestDto.getNickname())) {
-            throw new CustomException(NICKNAME_ALREADY_EXISTS);
+    private void validateDuplicateEmail(String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new CustomException(EMAIL_ALREADY_EXISTS);
         }
+    }
+
+    public void signupInfo(UserDto.SignUpInfoRequest requestDto) {
+        validateDuplicateEmail(requestDto.getEmail());
+        validateDuplicateNickname(requestDto.getNickname());
         requestDto.encodePassword(passwordEncoder.encode(requestDto.getPassword()));
         userRepository.save(requestDto.toEntity(null));
+    }
+
+    private void validateDuplicateNickname(String nickname) {
+        if (userRepository.existsByNickname(nickname)) {
+            throw new CustomException(NICKNAME_ALREADY_EXISTS);
+        }
     }
 }
