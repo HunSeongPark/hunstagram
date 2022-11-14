b4ca9eba39ddaf6aad974e6c194ec21b2f6f45eb
HunSeongPark <phunseong@gmail.com>
2022-11-14 20:23:55 +0900

    feat: SignUpInfoRequest validation 추가

--- src/main/java/com/example/hunstagram/domain/user/dto/UserDto.java
+++ src/main/java/com/example/hunstagram/domain/user/dto/UserDto.java
@@ -2,8 +2,10 @@ package com.example.hunstagram.domain.user.dto;
 
 import com.example.hunstagram.domain.user.entity.User;
 import lombok.*;
+import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -25,7 +27,7 @@ public class UserDto {
 
         @NotBlank(message = "비밀번호를 입력해주세요.")
         @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}",
-                message = "영어, 숫자, 특수문자를 포함하여 8~15자로 입력해주세요.")
+                message = "비밀번호는 영어, 숫자, 특수문자를 포함하여 8~15자로 입력해주세요.")
         private String password;
     }
 
@@ -44,11 +46,29 @@ public class UserDto {
     @AllArgsConstructor(access = PRIVATE)
     @Getter
     public static class SignUpInfoRequest {
+
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
         private String email;
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}",
+                message = "비밀번호는 영어, 숫자, 특수문자를 포함하여 8~15자로 입력해주세요.")
         private String password;
+
         private String profileImage;
+
+        @NotBlank(message = "이름을 입력해주세요.")
+        @Pattern(regexp = "[a-z가-힣1-9]{2,18}",
+                message = "이름은 영어, 한글, 숫자로 구성된 2~18자로 입력해주세요.")
         private String name;
+
+        @NotBlank(message = "닉네임을 입력해주세요.")
+        @Pattern(regexp = "[a-z1-9_]{4,12}",
+                message = "닉네임은 영어, 숫자, _로 구성된 4~10자로 입력해주세요.")
         private String nickname;
+
+        @Max(value = 30, message = "소개글은 최대 30자까지 입력 가능합니다.")
         private String introText;
 
         public void encodePassword(String encodedPassword) {
