1390f251cd7d9947c2b9815e2c78f1376657b050
HunSeongPark <phunseong@gmail.com>
2022-11-14 14:38:09 +0900

    feat: ExceptionHandler - Http Method Exception handle

--- src/main/java/com/example/hunstagram/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/hunstagram/global/exception/CustomExceptionHandler.java
@@ -2,10 +2,14 @@ package com.example.hunstagram.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static com.example.hunstagram.global.exception.CustomErrorCode.INVALID_HTTP_METHOD;
+import static com.example.hunstagram.global.exception.CustomErrorCode.INVALID_REQUEST;
 
 /**
  * @author : Hunseong-Park
@@ -15,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
+    // CustomException
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e, HttpServletRequest request) {
         log.error("url: {} | errorCode: {} | errorMessage: {} | cause Exception: ",
@@ -24,4 +29,19 @@ public class CustomExceptionHandler {
                 .status(e.getErrorCode().getHttpStatus())
                 .body(new ErrorResponse(e));
     }
+
+    // Not Support Http Method Exception
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMethodException(
+            HttpRequestMethodNotSupportedException e,
+            HttpServletRequest request
+    ) {
+        log.error("url: {} | errorCode: {} | errorMessage: {} | cause Exception: ",
+                request.getRequestURL(), INVALID_HTTP_METHOD, INVALID_HTTP_METHOD.getErrorMessage(), e);
+
+        return ResponseEntity
+                .status(INVALID_HTTP_METHOD.getHttpStatus())
+                .body(new ErrorResponse(INVALID_HTTP_METHOD));
+    }
+
 }
