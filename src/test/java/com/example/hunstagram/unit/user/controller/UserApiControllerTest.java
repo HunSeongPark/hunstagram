46091ce7f7b394fa4f826da0aea8c6074fdf56a9
HunSeongPark <phunseong@gmail.com>
2022-11-14 17:51:12 +0900

    test: UserControllerTest - [POST] 회원가입1 - id, pw

---
+++ src/test/java/com/example/hunstagram/unit/user/controller/UserApiControllerTest.java
@@ -0,0 +1,85 @@
+package com.example.hunstagram.domain.user.controller;
+
+import com.example.hunstagram.domain.user.dto.UserDto;
+import com.example.hunstagram.domain.user.entity.UserRepository;
+import com.example.hunstagram.domain.user.service.UserService;
+import com.example.hunstagram.global.security.SecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.annotation.security.RunAs;
+import java.nio.charset.StandardCharsets;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-11-14
+ */
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(controllers = UserApiController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
+class UserApiControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @DisplayName("[POST] 회원가입1 - id, pw")
+    @WithMockUser
+    @Test
+    void signup() throws Exception {
+
+        // given
+        String email = "gnstjd0831@naver.com";
+        String password = "test123456!";
+        UserDto.SignUpRequest requestDto = new UserDto.SignUpRequest(email, password);
+        String body = mapper.writeValueAsString(requestDto);
+
+        given(userService.signup(any()))
+                .willReturn(UserDto.SignUpResponse.fromRequestDto(requestDto));
+
+        // when & then
+        mvc.perform(post("/v1/users/signup")
+                        .content(body)
+                        .contentType(APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.email").value(email))
+                .andExpect(jsonPath("$.password").value(password));
+    }
+}
