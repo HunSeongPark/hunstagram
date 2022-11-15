c9577f41786d6d4327a7ec2c0fc7ebf93f5cc4ee
HunSeongPark <phunseong@gmail.com>
2022-11-15 19:52:52 +0900

    feat: AwsConfig

---
+++ src/main/java/com/example/hunstagram/global/aws/AwsConfig.java
@@ -0,0 +1,37 @@
+package com.example.hunstagram.global.aws;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-11-14
+ */
+@PropertySource("classpath:application-aws.yml")
+@Configuration
+public class AwsConfig {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+    }
+}
