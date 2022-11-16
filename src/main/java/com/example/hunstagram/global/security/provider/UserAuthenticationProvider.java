b0fac4aa0960d76ec6b1a77fc307f3dec6c3d4c6
HunSeongPark <phunseong@gmail.com>
2022-11-16 16:27:43 +0900

    feat: UserAuthenticationProvider

---
+++ src/main/java/com/example/hunstagram/global/security/provider/UserAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package com.example.hunstagram.global.security.provider;
+
+import com.example.hunstagram.global.security.service.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-11-16
+ */
+@RequiredArgsConstructor
+@Component
+public class UserAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = (String) authentication.getCredentials();
+
+        CustomUserDetails userDetails
+                = (CustomUserDetails) userDetailsService.loadUserByUsername(username);
+
+        // PW Check
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
