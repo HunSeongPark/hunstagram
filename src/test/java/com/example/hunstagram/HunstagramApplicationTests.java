31c6a032d23bc0a8e522055e822d475918a943d4
HunSeongPark <phunseong@gmail.com>
2022-11-16 15:54:51 +0900

    test: UserService - 회원가입 정보 입력 및 생성 유닛/통합테스트

--- src/test/java/com/example/hunstagram/HunstagramApplicationTests.java
+++ src/test/java/com/example/hunstagram/HunstagramApplicationTests.java
@@ -4,8 +4,10 @@ import com.example.hunstagram.config.AwsS3MockConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @Import(AwsS3MockConfig.class)
+@ActiveProfiles("test")
 @SpringBootTest
 class HunstagramApplicationTests {
 
