823c15325ea05c00fdfc768fb21bcd70addea1b4
HunSeongPark <phunseong@gmail.com>
2022-11-16 17:02:49 +0900

    feat: SecurityConfig - 로그인 필터 적용

--- src/main/java/com/example/hunstagram/global/security/SecurityConfig.java
+++ src/main/java/com/example/hunstagram/global/security/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.example.hunstagram.global.security;
 
+import com.example.hunstagram.global.security.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -18,15 +27,36 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @Configuration
 public class SecurityConfig {
 
+    private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationSuccessHandler successHandler;
+    private final AuthenticationFailureHandler failureHandler;
+    private final CustomAuthenticationFilter authenticationFilter;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 로그인 인증 필터
+        authenticationFilter.setFilterProcessesUrl("/v1/users/login");
+        authenticationFilter.setAuthenticationSuccessHandler(successHandler);
+        authenticationFilter.setAuthenticationFailureHandler(failureHandler);
+
         http.csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(STATELESS) // Using JWT
                 .and()
+                .addFilter(authenticationFilter)
+                .authenticationProvider(authenticationProvider)
                 .authorizeRequests()
                 .anyRequest().permitAll();
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        return auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder).and().build();
     }
 }
--- src/main/java/com/example/hunstagram/global/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/hunstagram/global/security/filter/CustomAuthenticationFilter.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +16,9 @@ import javax.servlet.http.HttpServletResponse;
  * @date : 2022-11-16
  */
 @RequiredArgsConstructor
+@Component
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
     private final AuthenticationManager authenticationManager;
 
     @Override
