fa31da3809422e847f6851cb2afcdebac034ac14
HunSeongPark <phunseong@gmail.com>
2022-11-16 17:15:50 +0900

    feat: 로그인 성공 이후 Refresh Token 저장 로직 추가

--- src/main/java/com/example/hunstagram/global/security/handler/UserSuccessHandler.java
+++ src/main/java/com/example/hunstagram/global/security/handler/UserSuccessHandler.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -32,6 +33,7 @@ public class UserSuccessHandler implements AuthenticationSuccessHandler {
     private final UserRepository userRepository;
     private final JwtService jwtService;
 
+    @Transactional
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
@@ -41,6 +43,7 @@ public class UserSuccessHandler implements AuthenticationSuccessHandler {
         // accessToken , refreshToken 생성
         String accessToken = jwtService.createAccessToken(user.getEmail(), USER, user.getId());
         String refreshToken = jwtService.createRefreshToken(user.getEmail());
+        user.updateRefreshToken(refreshToken);
         new ObjectMapper().writeValue(response.getWriter(), new TokenResponseDto(accessToken, refreshToken));
     }
 
