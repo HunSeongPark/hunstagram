b325a7f2f30ef572967c277fe47c83d2bf7157e4
HunSeongPark <phunseong@gmail.com>
2022-11-16 14:57:38 +0900

    feat: AWS S3 테스트를 위한 MockS3 설정

--- src/test/java/com/example/hunstagram/HunstagramApplicationTests.java
+++ src/test/java/com/example/hunstagram/HunstagramApplicationTests.java
@@ -1,8 +1,11 @@
 package com.example.hunstagram;
 
+import com.example.hunstagram.config.AwsS3MockConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(AwsS3MockConfig.class)
 @SpringBootTest
 class HunstagramApplicationTests {
 
