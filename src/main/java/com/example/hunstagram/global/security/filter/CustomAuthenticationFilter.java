24a856bda6f3bcecd42f6cefffd6fdcd4064f016
HunSeongPark <phunseong@gmail.com>
2022-11-16 16:30:30 +0900

    feat: CustomAuthenticationFilter

---
+++ src/main/java/com/example/hunstagram/global/security/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.example.hunstagram.global.security.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author : Hunseong-Park
+ * @date : 2022-11-16
+ */
+@RequiredArgsConstructor
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        Authentication token = new UsernamePasswordAuthenticationToken(email, password);
+        return authenticationManager.authenticate(token);
+    }
+}
