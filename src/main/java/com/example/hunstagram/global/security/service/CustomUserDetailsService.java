0a21962fb3f02e028ff86c715995d8f5dfc49960
HunSeongPark <phunseong@gmail.com>
2022-11-16 16:27:43 +0900

    feat: UserAuthenticationProvider

--- src/main/java/com/example/hunstagram/global/security/service/CustomUserDetailsService.java
+++ src/main/java/com/example/hunstagram/global/security/service/CustomUserDetailsService.java
@@ -18,7 +18,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(username)
