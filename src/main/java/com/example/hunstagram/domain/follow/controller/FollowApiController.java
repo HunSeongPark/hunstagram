5ea1cc4cb359c21cb4241e338ad9557c8b37cc77
HunSeongPark <phunseong@gmail.com>
2022-11-21 16:49:23 +0900

    feat: 팔로우 추가,취소 엔드포인트 추가

--- src/main/java/com/example/hunstagram/domain/follow/controller/FollowApiController.java
+++ src/main/java/com/example/hunstagram/domain/follow/controller/FollowApiController.java
@@ -2,6 +2,9 @@ package com.example.hunstagram.domain.follow.controller;
 
 import com.example.hunstagram.domain.follow.service.FollowService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class FollowApiController {
 
     private final FollowService followService;
+
+    @PostMapping("/{followerId}")
+    public ResponseEntity<FollowDto.Response> follow(@PathVariable Long followerId) {
+        return ResponseEntity.ok(followService.follow(followerId));
+    }
 }
