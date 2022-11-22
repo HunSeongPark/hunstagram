3c5bf28d217e765ecf763934f52050a8fb3774c4
HunSeongPark <phunseong@gmail.com>
2022-11-22 16:40:38 +0900

    feat: FollowListResponseDto

--- src/main/java/com/example/hunstagram/domain/follow/dto/FollowDto.java
+++ src/main/java/com/example/hunstagram/domain/follow/dto/FollowDto.java
@@ -1,7 +1,11 @@
 package com.example.hunstagram.domain.follow.dto;
 
+import com.example.hunstagram.domain.user.entity.User;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import static lombok.AccessLevel.PRIVATE;
 
 /**
  * @author : Hunseong-Park
@@ -14,5 +18,17 @@ public class FollowDto {
     public static class FollowResponse {
         // true 시 팔로우 추가, false 시 팔로우 취소
         private Boolean isFollowAdd;
+    }
+
+    @Getter
+    @AllArgsConstructor(access = PRIVATE)
+    public static class FollowListResponse {
+        private String name;
+        private String nickname;
+        private String profileImage;
+
+        public static FollowListResponse fromEntity(User user) {
+            return new FollowListResponse(user.getName(), user.getNickname(), user.getProfileImage());
+        }
     }
 }
