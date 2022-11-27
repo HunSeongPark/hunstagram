4ca0b24bd827647e4e6e3aeb7b0dba2d8c2fec26
HunSeongPark <phunseong@gmail.com>
2022-11-27 15:11:26 +0900

    test: 내 프로필 조회 - Service 유닛테스트

--- src/test/java/com/example/hunstagram/unit/user/service/UserServiceTest.java
+++ src/test/java/com/example/hunstagram/unit/user/service/UserServiceTest.java
@@ -1,10 +1,15 @@
 package com.example.hunstagram.unit.user.service;
 
+import com.example.hunstagram.domain.follow.entity.FollowRepository;
+import com.example.hunstagram.domain.post.entity.Post;
+import com.example.hunstagram.domain.post.entity.PostRepository;
 import com.example.hunstagram.domain.user.dto.UserDto;
+import com.example.hunstagram.domain.user.entity.User;
 import com.example.hunstagram.domain.user.entity.UserRepository;
 import com.example.hunstagram.domain.user.service.UserService;
 import com.example.hunstagram.global.aws.service.AwsS3Service;
 import com.example.hunstagram.global.exception.CustomException;
+import com.example.hunstagram.global.security.service.JwtService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +17,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+import java.util.Optional;
 
 import static com.example.hunstagram.global.exception.CustomErrorCode.EMAIL_ALREADY_EXIST;
 import static com.example.hunstagram.global.exception.CustomErrorCode.NICKNAME_ALREADY_EXIST;
@@ -34,10 +42,29 @@ class UserServiceTest {
     UserRepository userRepository;
 
     @Mock
+    PostRepository postRepository;
+
+    @Mock
+    FollowRepository followRepository;
+
+    @Mock
+    JwtService jwtService;
+
+    @Mock
     AwsS3Service awsS3Service;
 
     @Mock
     PasswordEncoder passwordEncoder;
+
+    private User createUser(Long id) {
+        return User.builder()
+                .id(id)
+                .email("test" + id + "@test.com")
+                .password("test123!" + id)
+                .name("test" + id)
+                .nickname("test" + id)
+                .build();
+    }
 
     @DisplayName("회원가입을 위한 email, pw 입력에 성공한다")
     @Test
@@ -117,5 +144,71 @@ class UserServiceTest {
         // when & then
         CustomException e = assertThrows(CustomException.class, () -> userService.signupInfo(requestDto, null));
         assertThat(e.getErrorCode()).isEqualTo(NICKNAME_ALREADY_EXIST);
+    }
+
+    @DisplayName("내 프로필 조회에 성공한다")
+    @Test
+    void get_my_profile_success() {
+
+        // given
+        User me = createUser(1L);
+
+        given(userRepository.findByEmail(any())).willReturn(Optional.of(me));
+
+        Post post = Post.builder()
+                .id(1L)
+                .thumbnailImage("test")
+                .build();
+
+        given(postRepository.findAllByUserId(any())).willReturn(List.of(post));
+        Integer followerCount = 918;
+        Integer followingCount = 1920;
+        given(followRepository.countFolloweeByUserId(any())).willReturn(followerCount);
+        given(followRepository.countFollowingByUserId(any())).willReturn(followingCount);
+
+        // when
+        UserDto.MyProfileResponse response = userService.getMyProfile();
+
+        // then
+        assertThat(response.getUserId()).isEqualTo(me.getId());
+        assertThat(response.getNickname()).isEqualTo(me.getNickname());
+        assertThat(response.getName()).isEqualTo(me.getName());
+        assertThat(response.getPostThumbnails().get(0).getPostId()).isEqualTo(post.getId());
+        assertThat(response.getPostThumbnails().get(0).getImagePath()).isEqualTo(post.getThumbnailImage());
+        assertThat(response.getFollowerCount()).isEqualTo(followerCount.toString());
+        assertThat(response.getFollowingCount()).isEqualTo("1.92K");
+    }
+
+    @DisplayName("내 프로필 조회에 성공한다 2")
+    @Test
+    void get_my_profile_success2() {
+
+        // given
+        User me = createUser(1L);
+
+        given(userRepository.findByEmail(any())).willReturn(Optional.of(me));
+
+        Post post = Post.builder()
+                .id(1L)
+                .thumbnailImage("test")
+                .build();
+
+        given(postRepository.findAllByUserId(any())).willReturn(List.of(post));
+        Integer followerCount = 9957;
+        Integer followingCount = 58201;
+        given(followRepository.countFolloweeByUserId(any())).willReturn(followerCount);
+        given(followRepository.countFollowingByUserId(any())).willReturn(followingCount);
+
+        // when
+        UserDto.MyProfileResponse response = userService.getMyProfile();
+
+        // then
+        assertThat(response.getUserId()).isEqualTo(me.getId());
+        assertThat(response.getNickname()).isEqualTo(me.getNickname());
+        assertThat(response.getName()).isEqualTo(me.getName());
+        assertThat(response.getPostThumbnails().get(0).getPostId()).isEqualTo(post.getId());
+        assertThat(response.getPostThumbnails().get(0).getImagePath()).isEqualTo(post.getThumbnailImage());
+        assertThat(response.getFollowerCount()).isEqualTo("9.96K");
+        assertThat(response.getFollowingCount()).isEqualTo("5.82M");
     }
 }
