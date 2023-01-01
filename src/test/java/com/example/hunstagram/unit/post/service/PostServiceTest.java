aa209639d87b4c0fcd3224f12c747bfcd92e7d36
HunSeongPark <phunseong@gmail.com>
2023-01-01 14:54:42 +0900

    test: PostService - 게시글 좋아요 유닛테스트

--- src/test/java/com/example/hunstagram/unit/post/service/PostServiceTest.java
+++ src/test/java/com/example/hunstagram/unit/post/service/PostServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.hunstagram.unit.post.service;
 
 import com.example.hunstagram.domain.hashtag.entity.HashtagRepository;
+import com.example.hunstagram.domain.like.dto.LikeDto;
+import com.example.hunstagram.domain.like.entity.Like;
+import com.example.hunstagram.domain.like.entity.LikeRepository;
 import com.example.hunstagram.domain.post.dto.PostDto;
 import com.example.hunstagram.domain.post.entity.Post;
 import com.example.hunstagram.domain.post.entity.PostRepository;
@@ -59,6 +62,9 @@ public class PostServiceTest {
 
     @Mock
     PostImageRepository postImageRepository;
+
+    @Mock
+    LikeRepository likeRepository;
 
     @DisplayName("post 등록에 성공한다 (content, hashtag 존재)")
     @Test
@@ -309,5 +315,101 @@ public class PostServiceTest {
         CustomException e = assertThrows(CustomException.class,
                 () -> postService.deletePost(1L));
         assertThat(e.getErrorCode()).isEqualTo(POST_NOT_FOUND);
+    }
+
+    @DisplayName("post 좋아요에 성공한다 - 추가")
+    @Test
+    void add_like_post_success() {
+
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .password("test12345!")
+                .name("test")
+                .nickname("test")
+                .build();
+        PostImage postImage = new PostImage("test", null);
+        Post post = Post.builder()
+                .id(1L)
+                .postImages(List.of(postImage))
+                .user(user)
+                .build();
+        given(postRepository.findById(any())).willReturn(Optional.of(post));
+        given(userRepository.findById(any())).willReturn(Optional.of(user));
+        given(likeRepository.findByPostAndUserId(any(), any())).willReturn(Optional.empty());
+
+        // when
+        LikeDto.Response response = postService.like(post.getId());
+
+        // then
+        assertThat(response.getIsLikeAdd()).isTrue();
+    }
+
+    @DisplayName("post 좋아요에 성공한다 - 취소")
+    @Test
+    void cancel_like_post_success() {
+
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .password("test12345!")
+                .name("test")
+                .nickname("test")
+                .build();
+        PostImage postImage = new PostImage("test", null);
+        Post post = Post.builder()
+                .id(1L)
+                .postImages(List.of(postImage))
+                .user(user)
+                .build();
+        given(postRepository.findById(any())).willReturn(Optional.of(post));
+        given(userRepository.findById(any())).willReturn(Optional.of(user));
+        given(likeRepository.findByPostAndUserId(any(), any())).willReturn(Optional.of(Like.builder().build()));
+
+        // when
+        LikeDto.Response response = postService.like(post.getId());
+
+        // then
+        assertThat(response.getIsLikeAdd()).isFalse();
+    }
+
+    @DisplayName("post 좋아요 시 게시글이 존재하지 않으면 실패한다")
+    @Test
+    void like_post_not_found_fail() {
+
+        // given
+        given(postRepository.findById(any())).willReturn(Optional.empty());
+
+        // when & then
+        CustomException e = assertThrows(CustomException.class, () -> postService.like(1L));
+        assertThat(e.getErrorCode()).isEqualTo(POST_NOT_FOUND);
+    }
+
+    @DisplayName("post 좋아요 시 사용자가 존재하지 않으면 실패한다")
+    @Test
+    void like_post_user_not_found_fail() {
+
+        // given
+        User user = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .password("test12345!")
+                .name("test")
+                .nickname("test")
+                .build();
+        PostImage postImage = new PostImage("test", null);
+        Post post = Post.builder()
+                .id(1L)
+                .postImages(List.of(postImage))
+                .user(user)
+                .build();
+        given(postRepository.findById(any())).willReturn(Optional.of(post));
+        given(userRepository.findById(any())).willReturn(Optional.empty());
+
+        // when & then
+        CustomException e = assertThrows(CustomException.class, () -> postService.like(1L));
+        assertThat(e.getErrorCode()).isEqualTo(USER_NOT_FOUND);
     }
 }
