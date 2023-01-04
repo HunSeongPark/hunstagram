42b30f36642c9d08b956b45a8647ee2845a55724
HunSeongPark <phunseong@gmail.com>
2023-01-04 19:11:45 +0900

    feat: 댓글 좋아요 엔드포인트 추가

--- src/main/java/com/example/hunstagram/domain/comment/controller/CommentApiController.java
+++ src/main/java/com/example/hunstagram/domain/comment/controller/CommentApiController.java
@@ -1,7 +1,11 @@
 package com.example.hunstagram.domain.comment.controller;
 
 import com.example.hunstagram.domain.comment.service.CommentService;
+import com.example.hunstagram.domain.like.dto.LikeDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentApiController {
 
     private final CommentService commentService;
+
+    @PostMapping("/{commentId}/like")
+    public ResponseEntity<LikeDto.Response> commentLike(@PathVariable Long commentId) {
+        return ResponseEntity.ok(commentService.like(commentId));
+    }
 }
