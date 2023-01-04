495d9a335955342101a63aab6dc64e8636a330f7
HunSeongPark <phunseong@gmail.com>
2023-01-04 19:15:05 +0900

    feat: 댓글 좋아요 로직 구현

--- src/main/java/com/example/hunstagram/domain/comment/service/CommentService.java
+++ src/main/java/com/example/hunstagram/domain/comment/service/CommentService.java
@@ -1,9 +1,22 @@
 package com.example.hunstagram.domain.comment.service;
 
+import com.example.hunstagram.domain.comment.entity.Comment;
 import com.example.hunstagram.domain.comment.entity.CommentRepository;
+import com.example.hunstagram.domain.like.dto.LikeDto;
+import com.example.hunstagram.domain.like.entity.Like;
+import com.example.hunstagram.domain.like.entity.LikeRepository;
+import com.example.hunstagram.domain.post.entity.Post;
+import com.example.hunstagram.domain.user.entity.User;
+import com.example.hunstagram.domain.user.entity.UserRepository;
+import com.example.hunstagram.global.exception.CustomException;
+import com.example.hunstagram.global.security.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.example.hunstagram.global.exception.CustomErrorCode.*;
 
 /**
  * @author : Hunseong-Park
@@ -14,5 +27,30 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CommentService {
 
+    private final JwtService jwtService;
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final LikeRepository likeRepository;
+
+    public LikeDto.Response like(Long commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CustomException(COMMENT_NOT_FOUND));
+        User user = userRepository.findById(jwtService.getId())
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+        Optional<Like> like = likeRepository.findByCommentAndUserId(comment.getId(), user.getId());
+
+        // 좋아요 취소
+        if (like.isPresent()) {
+            likeRepository.delete(like.get());
+            return new LikeDto.Response(false);
+        } else {
+            // 좋아요 추가
+            Like newLike = Like.builder()
+                    .user(user)
+                    .comment(comment)
+                    .build();
+            likeRepository.save(newLike);
+            return new LikeDto.Response(true);
+        }
+    }
 }
